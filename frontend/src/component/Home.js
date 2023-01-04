7cfdb4320659cf61442cc5f3beebace48592f991
sksdesilva <kavindusandeepadesilva@gmail.com>
2023-01-04 19:08:47 +0530

    modified

---
+++ frontend/src/component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ViewOrders from './ViewOrders'
+
+const Home = () => {
+  return (
+    <div>
+        <ViewOrders/>
+      
+    </div>
+  )
+}
+
+export default Home
