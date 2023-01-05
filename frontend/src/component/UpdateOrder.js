617431cb2c51bf9aa18e777a6455dd25126d7630
sksdesilva <kavindusandeepadesilva@gmail.com>
2023-01-05 20:10:16 +0530

    Modified UpdateOrder.js

--- frontend/src/component/UpdateOrder.js
+++ frontend/src/component/UpdateOrder.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 const UpdateOrder = () => {
     /*rafce - code snippet*/
 
+const[id,setId]=useState("");   
 const[cusName,setName]=useState("");
 const[foodName,setFoodName]=useState("");
 const[quntity,setQuntity]=useState("");
 const[orderTime,setOrderTime] = useState("");
 
 
-const { _id } = useParams();
-console.log({_id});
+
 
 function createOrder(event){
     event.preventDefault();
@@ -24,7 +24,7 @@ function createOrder(event){
         orderTime
     }
     console.log(order);
-    axios.post('http://localhost:8000/api/management/order',order).then(()=>{
+    axios.patch(`http://localhost:8000/api/management/order/${id}`,order).then(()=>{
         alert("order is submitted");
 
 
@@ -33,32 +33,41 @@ function createOrder(event){
     })
 }
 
+useEffect(()=>{
+  setId(localStorage.getItem(`Id`));
+  setName(localStorage.getItem(`name`));
+  setFoodName(localStorage.getItem(`food`));
+  setQuntity(localStorage.getItem(`quntity`));
+  setOrderTime(localStorage.getItem(`time`));
+},[])
+
   return (
     <div className='container'>
+      <h2>Update Order</h2>
 
 <form onSubmit={createOrder}>
   <div className="form-group">
     <label for="exampleInputEmail1">Customer Name</label>
-    <input type="text" className="form-control" id="cusName" aria-describedby="emailHelp" placeholder="Enter Customer Name" onChange={(event)=>{
+    <input type="text" className="form-control" value={cusName} id="cusName" aria-describedby="emailHelp" placeholder="Enter Customer Name" onChange={(event)=>{
         setName(event.target.value)
     }}/>
     <small id="emailHelp" className="form-text text-muted">We'll never share your details with anyone else.</small>
   </div>
   <div className="form-group">
     <label for="exampleInputPassword1">Dish Type</label>
-    <input type="text" className="form-control" id="foodName" placeholder="Enter Dish type" onChange={(event)=>{
+    <input type="text" className="form-control" value={foodName} id="foodName" placeholder="Enter Dish type" onChange={(event)=>{
         setFoodName(event.target.value)
     }}/>
   </div>
   <div className="form-group">
     <label for="exampleInputPassword1">quntity</label>
-    <input type="text" className="form-control" id="quntity" placeholder="Enter Quntity" onChange={(event)=>{
+    <input type="text" className="form-control" value={quntity} id="quntity" placeholder="Enter Quntity" onChange={(event)=>{
         setQuntity(event.target.value);
     }}/>
   </div>
   <div className="form-group">
     <label for="exampleInputPassword1">Order time</label>
-    <input type="text" className="form-control" id="OrderTime" placeholder="Order Time" onChange={(event)=>{
+    <input type="text" className="form-control" value={orderTime} id="OrderTime" placeholder="Order Time" onChange={(event)=>{
         
         setOrderTime(event.target.value);
     }}/>
