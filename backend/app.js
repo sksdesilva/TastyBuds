d754939cd93171f6e36425c2661303725899cdbe
sksdesilva <kavindusandeepadesilva@gmail.com>
2023-01-13 21:06:46 +0530

    Added user route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./Database/connect');
 const app = express();
 const Order = require('./Routes/Order')
+const User = require('./Routes/User');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const cors = require('cors');
@@ -18,6 +19,7 @@ app.use(cors());
 
 
 app.use('/api/management/order',Order);
+app.use('/api/management/user',User);
 app.use(notFound);
 
 const port = 8000;
